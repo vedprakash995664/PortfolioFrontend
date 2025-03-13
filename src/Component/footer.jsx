4feffe9d5e187c4fb177c550b712315ff86b0604
Ved Prakash <vedprakash995664@gmail.com>
2025-03-13 07:58:48 +0530

    d

--- src/Component/footer.jsx
+++ src/Component/footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../Css/Footer.css'
+import { Link } from "react-router-dom";
 
 function Footer() {
     return (
@@ -7,7 +8,7 @@ function Footer() {
             <center><hr style={{ borderWidth: "3px", color: "#B415FF", }} />
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
                     <p className="by"> All &copy; copyrights reserved to me || 2025</p>
-                    <p className="by">Designed & Developed By Er. Ved Prakash <a href="/admin" style={{textDecoration:"none"}}>❤️</a></p>
+                    <p className="by">Designed & Developed By Er. Ved Prakash <Link to='/admin' style={{textDecoration:"none"}}>❤️</Link></p>
                 </div></center>
         </div>
     )
